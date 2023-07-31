153bdd0ca7dfeb571b4992378474029e22648666
Saurabh0703 <66934981+Saurabh0703@users.noreply.github.com>
2023-07-31 17:03:19 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
+}
 output "private_subnet_id_1" {
   value = aws_subnet.private-1.id
 }
